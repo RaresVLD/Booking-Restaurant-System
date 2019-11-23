2577f4bff424c7d17ce30caed846e55837f098c5
RaresVLD <steam_vlaiduc@yahoo.com>
2019-11-23 15:59:16 +0200

    test

--- gitscripts/pushtomaster.sh
+++ gitscripts/pushtomaster.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Pushing changes to master..."
 
-git merge master
-echo "Merge master done"
-
 git checkout master
 echo "Checkout master done"
 
 git merge work
 echo "Merge work done"
+
+git push
+echo "Push master done"

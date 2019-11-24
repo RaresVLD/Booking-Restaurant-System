f581827a8fe89ba13acf2ee3a545f8fcf22d9a35
mateiacsinte <48494024+mateiacsinte@users.noreply.github.com>
2019-11-24 18:20:34 +0200

    Refactored Project

---
+++ app/RestaurantServiceProvider/RestaurantServiceProvider/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantServiceProvider.ServiceRepository;
+using System.Collections.Generic;
+
+
+namespace RestaurantServiceProvider.Controllers {
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        public IUserSerivceRepository _user;
+
+        public UsersController(IUserSerivceRepository user)
+        {
+          
+            _user = user;
+        }
+
+
+        [HttpGet]
+        public ActionResult<User> Get()
+        {
+            List<User> users = _user.GetUsers();
+            return Ok(users);    
+        }
+    }
+}
